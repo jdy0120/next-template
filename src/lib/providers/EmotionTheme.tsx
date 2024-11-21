8da96328a60cf956a19754089dead7603bf143ec
Doyeonism <jdy0120120@gmail.com>
2024-11-21 15:18:53 +0900

    update: global style

--- src/lib/providers/EmotionTheme.tsx
+++ src/lib/providers/EmotionTheme.tsx
@@ -3,13 +3,19 @@
 import React from "react";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "../../styles/themes";
+import GlobalStyle from "../../styles/themes/global";
 
 interface EmotionThemeProps {
   children: React.ReactNode;
 }
 
 const EmotionTheme = ({ children }: EmotionThemeProps) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default EmotionTheme;
